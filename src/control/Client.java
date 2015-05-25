a0fa7648e8de6ddaba01e3b7c33943a6a004b5fd
tree <tree@GiacomoPC>
2015-05-25 23:32:48 +0200

    v 0.2

--- src/control/Client.java
+++ src/control/Client.java
@@ -1,6 +1,72 @@
 
 package control;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import model.Packet;
+
 public class Client {
+    
+    private String clientName;
+    private String serverName;
+    int port;
+    
+    private Socket client = null;
+    
+    public Client (String clientName, String serverName, int port) {
+        this.clientName = clientName;
+        this.serverName = serverName;
+        this.port = port;     
+    }
+    
+    public boolean connect () {
+        try {
+            this.client = new Socket(this.serverName, this.port);
+            return true;
+        }
+        catch (IOException ex) {
+            return false;
+        }
+    }
+    
+    public Packet sendReceive (Packet packet) {        
+        try {
+            // to server:
+            OutputStream outToServer = client.getOutputStream();
+            ObjectOutputStream out = new ObjectOutputStream(outToServer);
+
+            out.writeObject(packet);
+
+            out.close();
+            
+            // from server:
+            InputStream inFromServer = client.getInputStream();            
+            ObjectInputStream in = new ObjectInputStream(inFromServer);
+            
+            Packet p = (Packet) in.readObject();
+            
+            in.close();
+            
+            return p;
+            //client.close(); 
+        }
+        catch (IOException | ClassNotFoundException ex) {
+            return null;
+        }
+    }
+    
+    public boolean disconnect () {
+        try {
+            this.client.close();
+            return true;
+        }
+        catch (IOException ex) {
+            return false;
+        }
+    }
     
 }
--- src/control/Server.java
+++ src/control/Server.java
@@ -7,13 +7,20 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Server extends Thread {
     
     private ServerSocket serverSocket;
+    HashMap<String, String> idip;
+    ArrayList<String> messages;
 
     public Server(int port) throws IOException {
         serverSocket = new ServerSocket(port);
+        
+        this.idip = new HashMap<>();
+        this.messages = new ArrayList<>();
     }
 
     @Override
@@ -24,12 +31,13 @@ public class Server extends Thread {
                 
                 ObjectInputStream in = new ObjectInputStream(server.getInputStream());
                 
+                // Packet from sender
                 Packet data = (Packet) in.readObject();
                 
-                
+                Packet r = this.packetManager(data, server.getInetAddress().toString());                
                 
                 ObjectOutputStream out = new ObjectOutputStream(server.getOutputStream());
-                out.writeObject(/);
+                out.writeObject(r);
                 
                 server.close();
             }
@@ -39,6 +47,32 @@ public class Server extends Thread {
             catch (ClassNotFoundException ex) {
                 System.err.println("ERROR: Class Not Found!");
             }
+        }
+    }
+    
+    private Packet packetManager (Packet p, String ip) { 
+        // return Packet to p.getReceiver()        
+        
+        switch (p.getLastMessageIndex()) {
+            case -1:
+                // p.getSender() join conversation
+                this.idip.put(p.getSender(), ip);
+                return null;
+            case -2:
+                // p.getSender() leave conversation
+                this.idip.remove(p.getSender());
+                return null;
+            default:
+                if (p.getData() != null) {
+                    // p.getSender() send new messages
+                    this.messages.addAll(p.getData());
+                }
+                
+                ArrayList<String> oldMessages = new ArrayList<>();                
+                for (int i = p.getLastMessageIndex()+1; i < this.messages.size() - p.getData().size(); i++) {
+                    oldMessages.add(this.messages.get(i));
+                }
+                return new Packet(p.getSender(), p.getReceiver(), oldMessages, 0);
         }
     }
 }
